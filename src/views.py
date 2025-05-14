6cefcb2768165fafcaefc7ac57b9f5d11eeacdb0
RuslanZubrin <error.blin@gmail.com>
2025-05-14 10:35:26 +0300

    исправлена директория

--- src/views.py
+++ src/views.py
@@ -1,13 +1,11 @@
 from datetime import datetime
-from .utils import (
+from src.utils import (
     get_greeting,
     get_card_data,
     get_top_transactions,
     get_currency_rates,
     get_stock_prices,
 )
-
-
 def main_function(date_time_str):
     current_time = datetime.strptime(date_time_str, "%Y-%m-%d %H:%M:%S")
     greeting = get_greeting(current_time)
@@ -22,5 +20,8 @@ def main_function(date_time_str):
         "currency_rates": currency_rates,
         "stock_prices": stock_prices,
     }
-
     return response  # Возвращаем response
+if __name__ == "__main__":
+    sample_date_time = "2024-06-01 12:00:00"
+    result = main_function(sample_date_time)
+    print(result)
